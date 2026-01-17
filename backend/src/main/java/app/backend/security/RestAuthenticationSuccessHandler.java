785179a3cbe1b8df1dbd0977cc9433beedad3d9b
Gabriela Plis <gabriela.plis.webdev@gmail.com>
2026-01-17 19:06:26 +0100

    update properties

--- backend/src/main/java/app/backend/security/RestAuthenticationSuccessHandler.java
+++ backend/src/main/java/app/backend/security/RestAuthenticationSuccessHandler.java
@@ -2,6 +2,7 @@ package app.backend.security;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
@@ -12,10 +13,13 @@ import java.io.IOException;
 @Component
 public class RestAuthenticationSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
 
+    @Value("${app.authentication.success-redirect-url}")
+    private String successRedirectUrl;
+
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException {
         clearAuthenticationAttributes(request);
-        getRedirectStrategy().sendRedirect(request, response, "/user");
+        getRedirectStrategy().sendRedirect(request, response, successRedirectUrl);
     }
 
 }
