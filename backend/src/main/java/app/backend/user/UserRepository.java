f78dcf795737ff77b6ee86f64b12956ee787b357
Gabriela Plis <gabriela.plis.webdev@gmail.com>
2026-01-17 18:25:57 +0100

    add native query

--- backend/src/main/java/app/backend/user/UserRepository.java
+++ backend/src/main/java/app/backend/user/UserRepository.java
@@ -1,12 +1,20 @@
 package app.backend.user;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.ListCrudRepository;
 
 import java.util.Optional;
 
 public interface UserRepository extends ListCrudRepository<UserEntity, Integer> {
 
+    @Query(
+        value = """
+                SELECT *
+                FROM users u
+                WHERE u.email = :email
+            """,
+        nativeQuery = true
+    )
     Optional<UserEntity> findByEmail(String email);
 
-    Optional<UserEntity> findByEmailAndPassword(String email, String password);
 }
